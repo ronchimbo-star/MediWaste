c9a3f47b7bf01ce49019be4158d9ee716f4e72db
ronchimbo-star <ronchimbo@gmail.com>
2026-09-25 19:38:40 +0100

    Update certificate logo to new asset

--- src/pages/admin/CertificatePreviewPage.tsx
+++ src/pages/admin/CertificatePreviewPage.tsx
@@ -41,7 +41,7 @@ export default function CertificatePreviewPage() {
   useEffect(() => {
     let cancelled = false;
     Promise.all([
-      imageToDataUrl('/image copy 3.png'),
+      imageToDataUrl('/image copy 4.png'),
       imageToDataUrl('/mediwaste-favicon.png'),
       imageToDataUrl('/signature.png'),
       imageToDataUrl('/mediwaste-logo-white.png'),
